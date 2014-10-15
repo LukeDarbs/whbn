22a12d1e417669fae21a63ed5afc410f273cbfbd
whbn <whbn@cb69510c-7c1f-45a7-b614-483a575112ab>
2014-10-15 11:11:09 +0000

    Site root definition tests…

--- trunk/public/variables.php
+++ trunk/public/variables.php
@@ -6,15 +6,25 @@ set_include_path( "inc/" );
 // http://blog.lavoie.sl/2013/02/php-document-root-path-and-url-detection.html
 // ==================================================================================
 
-	$base_dir  = __DIR__ ; // Absolute path to your installation, ex: /var/www/mywebsite
-	$doc_root  = preg_replace("!{$_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']); # ex: /var/www
-	$base_url  = preg_replace("!^{$doc_root}!", '', $base_dir); # ex: '' or '/mywebsite'
-	$protocol  = empty($_SERVER['HTTPS']) ? 'http' : 'https';
-	$port      = $_SERVER['SERVER_PORT'];
-	$disp_port = ($protocol == 'http' && $port == 80 || $protocol == 'https' && $port == 443) ? '' : ":$port";
-	$domain    = $_SERVER['SERVER_NAME'];
-	$full_url  = "$protocol://{$domain}{$disp_port}{$base_url}"; # Ex: 'http://example.com', 'https://example.com/mywebsite', etc.
-	$site_root = $full_url ;
+	// $base_dir  = __DIR__ ; // Absolute path to your installation, ex: /var/www/mywebsite
+	// $doc_root  = preg_replace("!{$_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']); # ex: /var/www
+	// $base_url  = preg_replace("!^{$doc_root}!", '', $base_dir); # ex: '' or '/mywebsite'
+	// $protocol  = empty($_SERVER['HTTPS']) ? 'http' : 'https';
+	// $port      = $_SERVER['SERVER_PORT'];
+	// $disp_port = ($protocol == 'http' && $port == 80 || $protocol == 'https' && $port == 443) ? '' : ":$port";
+	// $domain    = $_SERVER['SERVER_NAME'];
+	// $full_url  = "$protocol://{$domain}{$disp_port}{$base_url}"; # Ex: 'http://example.com', 'https://example.com/mywebsite', etc.
+	// $site_root = $full_url ;
+
+	$base_dir	= __FILE__; // Absolute path to your installation, ex: /var/www/mywebsite
+	$doc_root	= preg_replace("!{$_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']); # ex: /var/www
+	$base_url	= preg_replace("!^{$doc_root}!", '', $base_dir); # ex: '' or '/mywebsite'
+	$protocol	= empty($_SERVER['HTTPS']) ? 'http' : 'https';
+	$port 		= $_SERVER['SERVER_PORT'];
+	$disp_port	= ($protocol == 'http' && $port == 80 || $protocol == 'https' && $port == 443) ? '' : ":$port";
+	$domain		= $_SERVER['SERVER_NAME'];
+	$site_root	= "$protocol://{$domain}{$disp_port}{$base_url}"; # Ex: 'http://example.com', 'https://example.com/mywebsite', etc.
+	$site_root	= substr( $site_root, 0, -(strlen( "inc/vars.php" )));
 
 	global $site_root;
 
