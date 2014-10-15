34bce8eeb3b2202f6a9af2389ca80095a9fabc46
whbn <whbn@cb69510c-7c1f-45a7-b614-483a575112ab>
2014-10-15 10:56:40 +0000

    Site root definition tests…

--- trunk/public/variables.php
+++ trunk/public/variables.php
@@ -6,7 +6,7 @@ set_include_path( "inc/" );
 // http://blog.lavoie.sl/2013/02/php-document-root-path-and-url-detection.html
 // ==================================================================================
 
-	$base_dir  = __FILE__; // Absolute path to your installation, ex: /var/www/mywebsite
+	$base_dir  = __DIR__ ; // Absolute path to your installation, ex: /var/www/mywebsite
 	$doc_root  = preg_replace("!{$_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']); # ex: /var/www
 	$base_url  = preg_replace("!^{$doc_root}!", '', $base_dir); # ex: '' or '/mywebsite'
 	$protocol  = empty($_SERVER['HTTPS']) ? 'http' : 'https';
@@ -44,6 +44,10 @@ include("posts.php");
 				<li>
 					<h4>__FILE__</h4>
 					<p><?php echo __FILE__; ?></p>
+				</li>
+				<li>
+					<h4>__DIR__</h4>
+					<p><?php echo __DIR__; ?></p>
 				</li>
 				<li>
 					<h4>$base_dir</h4>
