8f43663e0e7cdbbab7a76102cb5ed4132852784b
whbn <whbn@cb69510c-7c1f-45a7-b614-483a575112ab>
2014-10-15 11:17:13 +0000

    Site root definition tests…

--- trunk/public/inc/variables.php
+++ trunk/public/inc/variables.php
@@ -16,15 +16,17 @@ set_include_path( "" );
 	// $full_url  = "$protocol://{$domain}{$disp_port}{$base_url}"; # Ex: 'http://example.com', 'https://example.com/mywebsite', etc.
 	// $site_root = $full_url ;
 
-	$base_dir	= __FILE__; // Absolute path to your installation, ex: /var/www/mywebsite
-	$doc_root	= preg_replace("!{$_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']); # ex: /var/www
-	$base_url	= preg_replace("!^{$doc_root}!", '', $base_dir); # ex: '' or '/mywebsite'
-	$protocol	= empty($_SERVER['HTTPS']) ? 'http' : 'https';
-	$port 		= $_SERVER['SERVER_PORT'];
-	$disp_port	= ($protocol == 'http' && $port == 80 || $protocol == 'https' && $port == 443) ? '' : ":$port";
-	$domain		= $_SERVER['SERVER_NAME'];
-	$site_root	= "$protocol://{$domain}{$disp_port}{$base_url}"; # Ex: 'http://example.com', 'https://example.com/mywebsite', etc.
-	$site_root	= substr( $site_root, 0, -(strlen( "inc/variables.php" )));
+	// $base_dir	= __FILE__; // Absolute path to your installation, ex: /var/www/mywebsite
+	// $doc_root	= preg_replace("!{$_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']); # ex: /var/www
+	// $base_url	= preg_replace("!^{$doc_root}!", '', $base_dir); # ex: '' or '/mywebsite'
+	// $protocol	= empty($_SERVER['HTTPS']) ? 'http' : 'https';
+	// $port 		= $_SERVER['SERVER_PORT'];
+	// $disp_port	= ($protocol == 'http' && $port == 80 || $protocol == 'https' && $port == 443) ? '' : ":$port";
+	// $domain		= $_SERVER['SERVER_NAME'];
+	// $site_root	= "$protocol://{$domain}{$disp_port}{$base_url}"; # Ex: 'http://example.com', 'https://example.com/mywebsite', etc.
+	// $site_root	= substr( $site_root, 0, -(strlen( "inc/variables.php" )));
+
+	$site_root	= "../";
 
 	global $site_root;
 
