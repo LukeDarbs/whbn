97ce21d8950ddb3606c181d6d69f39feb3f3d946
whbn <LukeDarbs@users.noreply.github.com>
2014-10-15 11:46:39 +0000

    Site root definition tests…

--- public/variables.php
+++ public/variables.php
@@ -2,13 +2,14 @@
 
 set_include_path( "inc/" );
 
-// Dynamically determine root directory regardless of server. Based on the excellent: 
-// http://blog.lavoie.sl/2013/02/php-document-root-path-and-url-detection.html
+// Dynamically determine root directory regardless of server…
 // ==================================================================================
 
+	// these two supplements from http://www.londatiga.net/it/how-to-define-global-absolute-path-in-php/
 	define('ROOT_DIR', dirname(__FILE__));
 	define('ROOT_URL', substr($_SERVER['PHP_SELF'], 0, - (strlen($_SERVER['SCRIPT_FILENAME']) - strlen(ROOT_DIR))));
 
+	// the rest based on http://blog.lavoie.sl/2013/02/php-document-root-path-and-url-detection.html
 	$base_dir  = __DIR__ ; // Absolute path to your installation, ex: /var/www/mywebsite
 	$doc_root  = preg_replace("!{$_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']); # ex: /var/www
 	$base_url  = preg_replace("!^{$doc_root}!", '', $base_dir); # ex: '' or '/mywebsite'
