52debc6f49d0944aa8b8b78e426014127925036c
whbn <whbn@cb69510c-7c1f-45a7-b614-483a575112ab>
2014-10-15 12:36:25 +0000

    Set default timezone in footer

--- trunk/public/inc/footer.php
+++ trunk/public/inc/footer.php
@@ -1,4 +1,4 @@
 
 		<footer>
-			<p>&copy; Luke Darbyshire <?php echo date("Y") ?> </p>
+			<p>&copy; Luke Darbyshire <?php date_default_timezone_set('Europe/London'); echo date("Y") ?> </p>
 		</footer>
