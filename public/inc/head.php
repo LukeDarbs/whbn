3fe1476d9b4dae2482c623756c8fa15d2632a0a6
LukeDarbs <luke@workhardbenice.co>
2019-03-07 16:52:20 +0000

    Fix broken favicon link

--- public/inc/head.php
+++ public/inc/head.php
@@ -3,13 +3,13 @@
 	<head>
 
 		<?php include_once("analyticstracking.php") ?>
-		
+
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="<?php echo $this_post['description']; ?>">
 		<meta name="author" content="Luke Darbyshire">
-		<link rel="icon" href="favicon.ico">
+		<link rel="icon" href="<?php echo $site_root; ?>favicon.ico">
 
 		<title>
 			<?php
